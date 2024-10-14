f7d464b501f68a43b65e01edd825b6769c4bae83
Meku-nint <154579159+Meku-nint@users.noreply.github.com>
2024-10-14 15:47:27 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+function calcage(birthyear){
+const age=2024-birthyear;
+console.log(firstname);
+function printage(){
+    const output=`${firstname},you are ${age},born in ${birthyear}`;
+    console.log(output);
+}
+printage();
+}
+const firstname="jonas";
+calcage(2002);
